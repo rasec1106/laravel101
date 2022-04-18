efd4964de1840414238f6e2804e81447ab00e109
rasec1106 <elch.herrera@gmail.com>
2022-04-18 16:34:30 -0500

    Add query params into the url

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,4 +10,12 @@ class TestController extends Controller
     function show(){
         return "SHOW";
     }
+    // To insert a function with a query param we can use the $
+    function params($id){
+        return "SHOW THE ID: ".$id;
+    }
+    // We set the $id to null because it's an optional param i the Routes
+    function blog($slug, $id=null){
+        return "The name of our post is ${slug} of id: ${id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 // New route to show a message
-Route::get('/hello', function(){
-    return "Hello world";
+// If we want to get the url variables (query params) we add the Request inside the function
+Route::get('/hello', function(Request $request){
+    return "Hello world".$request->get('variable');
 });
 // Instead of using a function as the second parameter we can use the keyword view
 Route::view('direct-view', 'myview',['name'=>'Cesar']);
 
 /**
  * To make a new controller we use this command: `php artisan make:controller {controller_name}`
+ * First we set the url, then an array that contains the controller class and the function
  */
 // Calling a controller
 Route::get('/controller',[TestController::class, 'show']);
-/**
- * First we set the url, then an array that contains the controller class and the function
- */
-Route::get('/controller2',[TestController::class, 'expert']);
+// We also can add the query params here
+Route::get('/params/{id}',[TestController::class, 'params']);
+// If a param is "optional" we can put a ? after the param
+Route::get('/blog/{slug}/{id?}',[TestController::class, 'blog']);
